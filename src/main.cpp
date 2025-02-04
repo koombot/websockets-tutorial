fc5bc73d55e55d8b28abc314eb9d691f560d53a3
koombot <84815933+koombot@users.noreply.github.com>
2025-02-04 10:21:07 +0000

    Successfully modified the Led.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,116 @@
+/**
+ * ----------------------------------------------------------------------------
+ * ESP32 Remote Control with WebSocket
+ * ----------------------------------------------------------------------------
+ * © 2020 Stéphane Calderoni
+ * ----------------------------------------------------------------------------
+ */
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// ----------------------------------------------------------------------------
+// Definition of macros
+// ----------------------------------------------------------------------------
+
+#define greenLedPin 26
+#define BTN_PIN 23
+
+// ----------------------------------------------------------------------------
+// Definition of global constants
+// ----------------------------------------------------------------------------
+
+const uint8_t DEBOUNCE_DELAY = 10; // in milliseconds
+
+// ----------------------------------------------------------------------------
+// Definition of the LED component will leave as a struct as basically everything will be public.  I need to access the on variable in the loop function.
+// ----------------------------------------------------------------------------
+
+struct Led {
+    // state variables
+    uint8_t pin;
+    bool    on;
+
+    Led(uint8_t pin, bool on) : pin(pin), on(on) {
+      pinMode(pin, OUTPUT);
+      update();
+    }
+
+    // methods
+    void update() {
+        digitalWrite(pin, on ? HIGH : LOW);
+    }
+};
+
+// ----------------------------------------------------------------------------
+// Definition of the Button component
+// ----------------------------------------------------------------------------
+
+struct Button {
+    // state variables
+    uint8_t  pin;
+    bool     lastReading;
+    uint32_t lastDebounceTime;
+    uint16_t state;
+
+    // methods determining the logical state of the button
+    bool pressed()                { return state == 1; }
+    bool released()               { return state == 0xffff; }
+    bool held(uint16_t count = 0) { return state > 1 + count && state < 0xffff; }
+
+    // method for reading the physical state of the button
+    void read() {
+        // reads the voltage on the pin connected to the button
+        bool reading = digitalRead(pin);
+
+        // if the logic level has changed since the last reading,
+        // we reset the timer which counts down the necessary time
+        // beyond which we can consider that the bouncing effect
+        // has passed.
+        if (reading != lastReading) {
+            lastDebounceTime = millis();
+        }
+
+        // from the moment we're out of the bouncing phase
+        // the actual status of the button can be determined
+        if (millis() - lastDebounceTime > DEBOUNCE_DELAY) {
+            // don't forget that the read pin is pulled-up
+            bool pressed = reading == LOW;
+            if (pressed) {
+                     if (state  < 0xfffe) state++;
+                else if (state == 0xfffe) state = 2;
+            } else if (state) {
+                state = state == 0xffff ? 0 : 0xffff;
+            }
+        }
+
+        // finally, each new reading is saved
+        lastReading = reading;
+    }
+};
+
+// ----------------------------------------------------------------------------
+// Definition of global variables
+// ----------------------------------------------------------------------------
+
+Led greenLed(greenLedPin, false);
+Button button = { BTN_PIN, HIGH, 0, 0 };
+
+// ----------------------------------------------------------------------------
+// Initialization
+// ----------------------------------------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(button.pin, INPUT);
 }
+
+// ----------------------------------------------------------------------------
+// Main control loop
+// ----------------------------------------------------------------------------
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    button.read();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (button.pressed()) greenLed.on = !greenLed.on;
+
+    greenLed.update();
 }
